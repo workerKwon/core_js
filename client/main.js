96c37a08899b25fe42ac9c296441058b6d8a3562
workerKwon <dmdmdm1322@gmail.com>
2024-11-20 13:32:17 +0900

    주접생성기

--- client/main.js
+++ client/main.js
@@ -1,3 +1,23 @@
-const a = "hello";
+import jujeobData from './data/data.js';
 
-alert("경고창!!");
+const submitButton = document.querySelector('#submit');
+submitButton.addEventListener('click', handleSubmit);
+
+const nameField = document.querySelector('#nameField');
+nameField.addEventListener('input', handleInput);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const name = nameField.value;
+  const jujeob = jujeobData(name);
+
+  const randomJujeob = jujeob[Math.floor(Math.random() * jujeob.length)];
+
+  const result = document.querySelector('.result');
+  result.textContent = randomJujeob;
+}
+
+function handleInput(event) {
+  console.log(event.target.value);
+}
